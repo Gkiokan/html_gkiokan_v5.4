6907ecd827d236dc214b6ecd9d55437ee189bdc5
Gkiokan <gkiokan@hotmail.de>
2016-11-06 16:08:17 +0100

    readd

---
+++ static/assets/js/countdown_vue.js
@@ -0,0 +1,10 @@
+var countdown = new Vue({
+    el : '#countdown_new_year',
+    data : {
+        time : {
+            d : 00,
+            m : 00,
+            y : 2016,
+        }
+    }
+});
---
+++ static/assets/js/footer.js
@@ -0,0 +1,3 @@
+$(function(){
+   countdown.run('#countdown_new_year', new Date('2017/01/01')) 
+});
