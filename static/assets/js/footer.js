9728d6d9c6cb172f43f4f20afd103bc0169fbc64
Gkiokan Sali <gkiokan@hotmail.de>
2016-11-28 00:05:47 +0100

    update for 1.4.0

--- static/assets/js/footer.js
+++ static/assets/js/footer.js
@@ -1,3 +1,9 @@
 $(function(){
-   countdown.run('#countdown_new_year', new Date('2017/01/01')) 
+    countdown.run('#countdown_new_year', new Date('2017/01/01'))
+
+    // lets do some magic for the header
+     setTimeout(function(){
+        if($(document).scrollTop()==0)
+        $('body').addClass('collapsed')        
+     }, 1300)
 });
